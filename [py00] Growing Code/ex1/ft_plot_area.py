604901b4694bdd7d8b44830bbefb388635f95f1d
2mdtln <120330170+2mdtln@users.noreply.github.com>
2026-02-09 16:00:22 +0300

    hello python written in car from ankara to istanbul :)

---
+++ [py00] Growing Code/ex1/ft_plot_area.py
@@ -0,0 +1,16 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                          :::      ::::::::   #
+#   ft_plot_area.py                                      :+:      :+:    :+:   #
+#                                                      +:+ +:+         +:+     #
+#   By: mtaheri <mtaheri@student.42istanbul.com.tr>  +#+  +:+       +#+        #
+#                                                  +#+#+#+#+#+   +#+           #
+#   Created: 2026/02/08 21:52:57 by mtaheri             #+#    #+#             #
+#   Updated: 2026/02/09 15:57:40 by mtaheri            ###   ########.fr       #
+#                                                                              #
+# **************************************************************************** #
+
+def ft_plot_area():
+	length = int(input("Enter length: "))
+	width = int(input("Enter width: "))
+	print("Plot area:", length * width)
