7ea061e72cff57bcf9bb2e66e9c9d2493bc629a2
2mdtln <120330170+2mdtln@users.noreply.github.com>
2026-02-19 20:22:22 +0300

    refac(py00): flake8

--- [py00] Growing Code/ex7/ft_seed_inventory.py
+++ [py00] Growing Code/ex7/ft_seed_inventory.py
@@ -6,10 +6,9 @@
 #   By: mtaheri <mtaheri@student.42istanbul.com.tr> +#+  +:+       +#+        #
 #                                                 +#+#+#+#+#+   +#+           #
 #   Created: 2026/02/13 17:31:01 by mtaheri            #+#    #+#             #
-#   Updated: 2026/02/19 20:13:20 by mtaheri           ###   ########.fr       #
+#   Updated: 2026/02/19 20:21:27 by mtaheri           ###   ########.fr       #
 #                                                                             #
 # *************************************************************************** #
-
 
 def last_part(quantity: int, unit: str) -> str:
     if (unit == "packets"):
@@ -19,8 +18,9 @@ def last_part(quantity: int, unit: str) -> str:
     elif (unit == "area"):
         return (f"covers {quantity} square meters")
 
+
 def ft_seed_inventory(seed_type: str, quantity: int, unit: str) -> None:
-    if (unit == "packets" or unit == "grams" or  unit == "area"):
+    if (unit == "packets" or unit == "grams" or unit == "area"):
         print(f"{seed_type.capitalize()} seeds: {last_part(quantity, unit)}")
     else:
         print("Unknown unit type")
